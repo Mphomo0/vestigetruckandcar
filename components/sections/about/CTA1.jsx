fda53f16a6374e32f280f9b572ade6f653a8e853
Mpho Moipolai <mpho.moipolai@autotrader.co.za>
2025-02-17 15:03:02 +0200

    first images link

--- components/sections/about/CTA1.jsx
+++ components/sections/about/CTA1.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function CTA1() {
   return (
-    <div className='w-full bg-[url(@/public/images/bg/13.jpg)] bg-cover bg-center bg-red-950 bg-blend-multiply'>
+    <div className='w-full bg-[url(/public/images/bg/13.jpg)] bg-cover bg-center bg-red-950 bg-blend-multiply'>
       <div className='max-w-7xl mx-auto px-8 md:px-6 lg:px-8 py-8'>
         <div className='text-white flex justify-between items-center'>
           <p className='lg:text-4xl text-md font-bold'>24 hr Towing Services</p>
--- components/sections/about/Testimonials.jsx
+++ components/sections/about/Testimonials.jsx
@@ -99,7 +99,7 @@ const Testimonials = () => {
   }, [])
 
   return (
-    <section className='w-full bg-[url(@/public/images/bg/8.jpg)] bg-cover bg-center'>
+    <section className='w-full bg-[url(/public/images/bg/8.jpg)] bg-cover bg-center'>
       <div className='mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:px-8 lg:py-16'>
         <h2 className='text-center text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl'>
           Read trusted reviews from our customers
